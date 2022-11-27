48bcd9b560c1f540e34394d792fbe59a915845c2
ken1flan <ken1flan@gmail.com>
2022-11-27 15:07:55 +0900

    ゲームに各コンテナを配置

--- lib/klondike_game.dart
+++ lib/klondike_game.dart
@@ -1,11 +1,59 @@
+import 'package:flame/experimental.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/widgets.dart';
+import 'package:klondike_flutter/components/foundation.dart';
+import 'package:klondike_flutter/components/stock.dart';
+import 'package:klondike_flutter/components/waste.dart';
+import 'package:klondike_flutter/components/pile.dart';
 
 class KlondikeGame extends FlameGame {
+  static const double cardWidth = 1000.0;
+  static const double cardHeight = 1400.0;
+  static const double cardGap = 175.0;
+  static const double carRadius = 100.0;
+  static final Vector2 cardSize = Vector2(cardWidth, cardHeight);
+
   @override
   Future<void> onLoad() async {
     await Flame.images.load('klondike-sprites.png');
+
+    final stock = Stock()
+      ..size = cardSize
+      ..position = Vector2(cardGap, cardGap);
+    final waste = Waste()
+      ..size = cardSize
+      ..position = Vector2(cardWidth + 2 * cardGap, cardGap);
+    final foundations = List.generate(
+      4,
+      (index) => Foundation()
+        ..size = cardSize
+        ..position =
+            Vector2((index + 3) * (cardWidth + cardGap) + cardGap, cardGap),
+    );
+    final piles = List.generate(
+      7,
+      (index) => Pile()
+        ..size = cardSize
+        ..position = Vector2(
+          cardGap + index * (cardWidth + cardGap),
+          cardHeight + 2 * cardGap,
+        ),
+    );
+
+    final world = World()
+      ..add(stock)
+      ..add(waste)
+      ..addAll(foundations)
+      ..addAll(piles);
+    add(world);
+
+    final camera = CameraComponent(world: world)
+      ..viewfinder.visibleGameSize =
+          Vector2(cardWidth * 7 + cardGap * 8, 4 * cardHeight + 3 * cardGap)
+      ..viewfinder.position = Vector2(cardWidth * 3.5 + cardGap * 4, 0)
+      ..viewfinder.anchor = Anchor.topCenter;
+    add(camera);
   }
 
   Sprite klondikeSprite(double x, double y, double width, double height) {
