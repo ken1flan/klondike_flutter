e28163e61ef36e437f1aec9c0d92abc2e50a49ac
ken1flan <ken1flan@gmail.com>
2023-01-15 00:52:02 +0900

    捨て山がカードを持つように変更

--- lib/components/waste_pile.dart
+++ lib/components/waste_pile.dart
@@ -1,6 +1,33 @@
 import 'package:flame/components.dart';
+import 'package:klondike_flutter/klondike_game.dart';
+import 'package:klondike_flutter/components/card.dart';
 
 class WastePile extends PositionComponent {
+  WastePile({super.position}) : super(size: KlondikeGame.cardSize);
+
   @override
   bool get debugMode => true;
+
+  final List<Card> _cards = [];
+  final Vector2 _fanOffset = Vector2(KlondikeGame.cardWidth * 0.2, 0);
+
+  void acquireCard(Card card) {
+    assert(card.isFaceUp);
+    card.position = position;
+    card.priority = _cards.length;
+    _cards.add(card);
+  }
+
+  void _fanOutTopCards() {
+    final n = _cards.length;
+    for (var i = 0; i < n; i++) {
+      _cards[i].position = position;
+    }
+    if (n == 2) {
+      _cards[1].position.add(_fanOffset);
+    } else if (n >= 3) {
+      _cards[n - 2].position.add(_fanOffset);
+      _cards[n - 1].position.addScaled(_fanOffset, 2);
+    }
+  }
 }
